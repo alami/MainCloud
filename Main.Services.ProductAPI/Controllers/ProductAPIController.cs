72f02212949c1f90193cf18ae6bcdff638051258
aminov <alami@tshtt.uz>
2021-09-16 14:02:07 +0500

    ch0308 1st contoller Products

---
+++ Main.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,36 @@
+﻿using Main.Services.ProductAPI.Models.Dto;
+using Main.Services.ProductAPI.Models.Dtos;
+using Main.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Main.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]
+    public class ProductAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IProductRepository _productRepository;
+        public ProductAPIController(IProductRepository productRepository)   
+        {
+            _productRepository = productRepository;
+            this._response = new ResponseDto();
+        }
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;
+            } catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() } ;
+            }
+            return _response;
+        }
+    }
+}
