ace7de061c7679d3b349c63f4e4697f792aae9d3
aminov <alami@tshtt.uz>
2021-09-17 13:37:58 +0500

    ch0315 generic

---
+++ Main.Web/Models/ApiRequest.cs
@@ -0,0 +1,13 @@
+﻿using static Main.Web.SD;
+
+namespace Main.Web.Models
+{
+    public class ApiRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data{ get; set; }
+        public string AccessToken { get; set; }
+
+    }
+}
---
+++ Main.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using Main.Web.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Main.Web.Services.IServices
+{
+    public interface IBaseService: IDisposable
+    {
+        ResponseDto responseModel {  get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
